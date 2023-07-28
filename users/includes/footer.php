519e298366a593b63a4f679b91c905b4892d9385
Vishvajeet017 <95349210+Vishvajeet017@users.noreply.github.com>
2023-07-28 15:23:46 +0530

    Add files via upload

---
+++ users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              2099921 - any queries contact support@vit.ac.in
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
